ae9fd31280dd349b3ff99140ddd341aa8f27e8f6
Alain Rico RAKOTONDRANAIVO <alainricor@gmail.com>
2023-04-02 21:38:24 +0300

    yes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,21 +14,13 @@ const HomePage = () => {
                     <div className="collapse navbar-collapse" id="navbarsExample04">
                         <ul className="navbar-nav ms-auto mb-2 mb-md-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link" aria-current="page" href="#">About us</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Link</a>
+                                <a className="nav-link" href="#">FAQ</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                                <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                                <a className="nav-link" href="#">Log In</a>
                             </li>
                         </ul>
                     </div>
